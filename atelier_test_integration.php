e2368230e56508ce0b33cb60b038732255c0ad53
Tristan Campbell <tcampbel@ucsd.edu>
2015-09-22 15:39:55 +0200

    Ajouter "SimpleTest" et fichier AtelierTestIntegration.php

---
+++ atelier_test_integration.php
@@ -0,0 +1,22 @@
+<?php
+  require_once(dirname(__FILE__) . 'simpletest/autorun.php');
+
+  class Atelier_Test_Integration extends UnitTestCase {
+    function testAjouterAtelier() {  
+    }
+
+    function testModifierAtelier() {
+    }
+
+    function testSupprimerAtelier() {
+    }
+
+    function testListerAtliers() {
+    }
+
+    function testListerAjouterModifierSupprimerAtelier() {
+    }
+
+  }
+
+?>
