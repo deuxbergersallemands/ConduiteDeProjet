a2fd1b83168e7e2718e6fd90678daa983f406497
Tristan Campbell <tcampbel@ucsd.edu>
2015-09-28 01:19:26 +0200

    E2E Test -- Non Optimisé
    Pour vérifier qu’un atelier sera ajouté, modifié, et puis supprimé dans
    cet ordre j’ai combiné toutes les fonctions dans un test — sinon on ne
    peut pas forcement vérifier que les fonctions seront exécutées dans cet
    ordre.  Mais les tests marchent :)

--- tst/atelier_test_integration.php
+++ tst/atelier_test_integration.php
@@ -1,22 +1,41 @@
 <?php
-  require_once(dirname(__FILE__) . 'simpletest/autorun.php');
+  require_once(dirname(__FILE__) . '/simpletest/autorun.php');
+  include('../REST/client/RestClient.class.php');
 
   class Atelier_Test_Integration extends UnitTestCase {
-    function testAjouterAtelier() {  
-    }
 
-    function testModifierAtelier() {
-    }
+    # Ajouter, Modifier, et puis Supprimer un atelier avec le client REST
+    function testE2E() {  
+      $client = new RestClient("http://raphael.jorel.emi.u-bordeaux.fr/REST/server");
+      $IDLePlusRecent = 0;
 
-    function testSupprimerAtelier() {
-    }
+      $nombreDAtliersInitial = count($client->getAteliers());
 
-    function testListerAtliers() {
-    }
+      // Ajouter un atelier
+      $client->addAtelier(array('titre' => "ExempleAjout"));
+      $ateliers = $client->getAteliers();
+      
+      $this->assertTrue( $nombreDAtliersInitial < count($ateliers) );
 
-    function testListerAjouterModifierSupprimerAtelier() {
-    }
+       # Parcourir les IDs pour trouver le ID le plus recent 
+       # Non-Optimisé
+      foreach ($ateliers as $atelier) {
+        if ($atelier->Id > $IDLePlusRecent)
+          $IDLePlusRecent = $atelier->Id;
+      }
 
+      // Modifier l'atelier
+      $client->updateAtelier($IDLePlusRecent, array('titre' => "ExempleAjoutModifie"));
+      $atelierModifie = $client->getAtelierByID($IDLePlusRecent);
+      $this->assertTrue($atelierModifie->Titre = "ExempleAjoutModifie");
+
+      // Supprimer un atelier
+      $client->delAtelier($IDLePlusRecent);
+      # Vérifier qu'il y a autant d'ateliers qu'avant le test
+      $this->AssertTrue($nombreDAtliersInitial === count($client->getAteliers()));
+      $this->AssertFalse($client->getAtelierByID($IDLePlusRecent)); 
+    }
   }
+
 
 ?>
